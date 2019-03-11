04a79c82ef62c37f0edaec62745cf52640b75b56
yellowBunny <yellow.bunny51@gmail.com>
2019-03-11 18:08:55 +0100

    added two virtual sensors

---
+++ sensors/virtual_DHT11.py
@@ -0,0 +1,25 @@
+import json, random
+
+class DHT11():
+
+    def grab_temp_and_humanidity(self, pin):
+        rand_temps = random.randrange(100)
+        rand_humanidity = random.randrange(100)
+        print(f'tu mamy pin: {pin}')
+        return rand_temps, rand_humanidity
+
+    def transform_to_dict(self, pin):
+        temps = self.grab_temp_and_humanidity(pin)
+        return {title: temp for title, temp in zip(['temp', 'humanidity'],temps)}
+
+    def json_temp(self, pin):
+        dict_data = self.transform_to_dict(pin)
+        json_encoder = json.JSONEncoder()
+        data = json_encoder.encode(dict_data)
+        # print(data)
+        return data
+
+
+if __name__ == '__main__':
+    instance = DHT11()
+    print(instance.json_temp(20))
---
+++ sensors/virtual_ds18b20.py
@@ -0,0 +1,20 @@
+import random, json
+
+class DS18b20():
+    def grab_temp(self, pin):
+        print(f'tu mamy pin: {pin}')
+        rand_temp = random.randrange(-50, 50)
+        return [rand_temp]
+
+    def transform_list_to_dict(self, pin):
+        temp_in_list = self.grab_temp(pin)
+        return {'temp' : temp for temp in temp_in_list}
+
+    def json_temp(self,pin):
+        temp_in_dict = self.transform_list_to_dict(pin)
+        json_encoder = json.JSONEncoder()
+        return json_encoder.encode(temp_in_dict)
+
+if __name__ == '__main__':
+    instance = DS18b20()
+    print(instance.json_temp(99))
