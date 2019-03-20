7d4cbccb4f54d274d966fe30a28a4378368e0c97
yellowBunny <yellowBunny51@gmail.com>
2019-03-20 11:14:22 +0100

    add css file

--- sensors/container_temp.py
+++ sensors/container_temp.py
@@ -2,18 +2,17 @@ import os
 print(os.getcwd())
 from sensors import dht11, ds18b20
 
-class Container():
+class Container():    
     def temp_containter_list(self):
 ##        self.outside = ds18b20.DS18b20.grab_temp()
         self.salon = dht11.DHT11().transform_to_dict(16)
         self.p1 = dht11.DHT11().transform_to_dict(12)
         self.kitchen = dht11.DHT11().transform_to_dict(7)
         self.p2 = dht11.DHT11().transform_to_dict(20)
-##        self.kitchen = virtual_DHT11.DHT11().transform_to_dict(16)
         self.bathroom = dht11.DHT11().transform_to_dict(21)
         container = [self.salon, self.p1, self.kitchen, self.p2, self.bathroom]
         for i in range(len(container)):
-            print(i+1, container[i])            
+            print(i+1, container[i])        
         return container
 
 
