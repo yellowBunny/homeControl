d2f2faee4b3244d221dfbaab50247cb83c60c9cb
yellowBunny <yellowBunny51@gmail.com>
2019-03-20 09:19:47 +0100

    added fifth sensor

--- sensors/container_temp.py
+++ sensors/container_temp.py
@@ -1,16 +1,20 @@
 import os
 print(os.getcwd())
-from sensors import dht11, ds18b20
+import dht11, ds18b20
 
 class Container():
     def temp_containter_list(self):
 ##        self.outside = ds18b20.DS18b20.grab_temp()
         self.salon = dht11.DHT11().transform_to_dict(16)
         self.p1 = dht11.DHT11().transform_to_dict(12)
-        self.p2 = dht11.DHT11().transform_to_dict(7)
+        self.kitchen = dht11.DHT11().transform_to_dict(7)
+        self.p2 = dht11.DHT11().transform_to_dict(20)
 ##        self.kitchen = virtual_DHT11.DHT11().transform_to_dict(16)
-##        self.bathroom = virtual_DHT11.DHT11().transform_to_dict(15)
-        return [self.salon, self.p1, self.p2]
+        self.bathroom = dht11.DHT11().transform_to_dict(21)
+        container = [self.salon, self.p1, self.kitchen, self.p2, self.bathroom]
+        for i in range(len(container)):
+            print(i+1, container[i])            
+        return container
 
 
 
